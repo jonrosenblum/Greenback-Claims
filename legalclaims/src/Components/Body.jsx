cd6a07f39c5a9c52899abed68b2e19bec3bf2028
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-11-17 00:50:47 -0500

    HEADER AND FOOTER

--- legalclaims/src/Components/Body.jsx
+++
@@ -1,9 +0,0 @@
-import ClaimForm from "./ClaimForm";
-
-export default function Body() {
-    return(
-        <div className="flex">
-            This is the body with the <ClaimForm/> element inside
-        </div>
-    );
-}
--- legalclaims/src/Components/ClaimForm.jsx
+++
@@ -1,5 +0,0 @@
-export default function ClaimForm() {
-    return(
-        <div>NICE FORM HERE</div>
-    );
-}
