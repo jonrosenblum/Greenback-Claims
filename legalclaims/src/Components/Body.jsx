4ac652cf68df893df6be8a01f3146e42fe57a3e0
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-11-17 00:03:02 -0500

    Tailwind Added / Basic Layout

---
+++ legalclaims/src/Components/Body.jsx
@@ -0,0 +1,9 @@
+import ClaimForm from "./ClaimForm";
+
+export default function Body() {
+    return(
+        <div className="flex">
+            This is the body with the <ClaimForm/> element inside
+        </div>
+    );
+}
