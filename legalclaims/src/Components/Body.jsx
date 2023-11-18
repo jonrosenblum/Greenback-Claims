77054f58fadfedb3a9fb2d73f42a0869fd8251e9
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-11-17 22:54:31 -0500

    UPDATES

--- legalclaims/src/Components/Body.jsx
+++ legalclaims/src/Components/Body.jsx
@@ -6,14 +6,14 @@ import { FaArrowRightLong } from "react-icons/fa6";
 export default function Body() {
     return(
         <div>
-            <section id="home">
+            <section>
             <div className="bg-gray-900 text-md">
                 <div className="py-20 px-4 sm:px-6 lg:px-8 bg-cover bg-center" style={{ backgroundImage: 'url("https://www.ncmic.com/webres/Image/0581-NCMIC-Website-Graphics_Finance_Credit-Card-Processing_Hero_1500x580.jpg")' }}>
                     <div className="max-w-xl">
                     <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-tight mb-4">Did You Own A Business Between 2004 and 2019?</h1>
                     <p className="text-md md:text-lg mb-8 max-w-sm font-medium">You Could be Eligible to Reclaim Funds as Part of a $5.5 Billion Dollar Settlement. </p>
-                    <a href="#form" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-2xl px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Submit Claim Now
+                    <a href="#form" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-2xl px-6 py-4 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Submit Claim
                     </a>
                     </div>
                 </div>
