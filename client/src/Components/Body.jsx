257e80a712911d086640608945e2da6e2a748f91
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-12-16 18:54:34 -0700

    ERC Updated

--- client/src/Components/Body.jsx
+++ client/src/Components/Body.jsx
@@ -56,15 +56,14 @@ export default function Body() {
                         </div>
                     </div>
                     <div className="block lg:flex bg-bgg">
-                        <div className="lg:w-1/2 p-2 text-white flex">
+                        <div className="lg:w-1/2 p-2 text-white flex items-center">
                             <div className="p-8 flex flex-col">
-                                <h2 className="text-xl font-bold mb-6">If your ERC ERC ERC ERC for payments at any point between 2004 and 2019, it&apos;s possible that you were charged excessive fees for credit card transactions.</h2>
-                                <p className="mb-6 text-sm">MasterCard and Visa faced legal action for excessively charging fees on credit card transactions during that period. A $5.5 billion settlement has been established, permitting eligible businesses to seek restitution for these fees. Our law firm specializes in class action cases and is dedicated to ensuring that your company receives its rightful compensation. </p>
-                                <p className="mb-6 text-sm">We handle the process for you: verifying eligibility, submitting claims accurately and punctually, and securing your payment directly upon approval. No fees are charged unless we successfully recover your funds. To begin, simply fill out the form, and we&apos;ll take care of the rest. Register today to potentially receive your reimbursement. For inquiries, contact us at claims@greenbackclaims.com.</p>
-                                <p className="mb-6 text-sm">NOTE: If your claim is accepted, the anticipated payout timeframe should be in 2024.  </p>
+                            <h2 className="text-[29px] font-bold mb-6">The Employee Retention Credit or ERC is a generous stimulus program designed to bolster businesses that were able to retain their employees during the Covid-19 Pandemic by giving them a refundable tax credit check.</h2>
+                                <p className="mb-6 text-base">This credit, spanning 2020 and the initial three quarters of 2021, allows eligible employers to claim up to $26,000 per employee. Qualification hinges on various factors, including experiencing significant declines in gross receipts or facing government-issued operational suspensions due to COVID-19. Eligible entities range from small businesses to larger corporations, encompassing a spectrum of industries. This credit presents a lifeline for those navigating economic disruptions, empowering businesses to sustain their workforce and operations, thereby fostering resilience during unprecedented times.</p>
+                                <p className="mb-6 text-base">Greenback Claims collaborates with legal experts to facilitate ERC claims for eligible businesses. Qualification for the ERC revolves around meeting specific criteria tied to revenue declines or operational restrictions due to COVID-19. Our partnership streamlines the process, ensuring accurate and timely submission of claims on behalf of businesses seeking reimbursement of up to $26,000 per employee. Our dedication lies in managing the intricate aspects of the claim process, offering guidance and expertise while eliminating upfront costs. For inquiries or assistance with your ERC claim, reach out to claims@greenbackclaims.com. We’re committed to securing the compensation your business deserves, navigating the complexities of the ERC to maximize your reimbursement.</p>
                                 <div className="flex items-center justify-between">
                                     <h3 className="text-2xl font-bold">Submit claim today:</h3>
-                                    <FaArrowRightLong className="text-4xl" />  
+                                    <FaArrowRightLong className="text-4xl" />
                                 </div>
                             </div>
                         </div>
