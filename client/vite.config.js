55fec62066cb767386c1407f15935439ebb68769
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-12-22 08:55:05 -0500

    VITE CONFIF UPDATED

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default {
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // Replace with your backend server address
+        target: 'http://api.greenbackclaims.com', // Replace with your backend server address
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), // Rewrite optional, if needed
       },
