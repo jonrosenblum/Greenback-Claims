748d4550ca20723b49739d608b953fe61deeca32
naeem113 <na6574113@gmail.com>
2023-12-28 22:16:34 +0500

    fix EIN number validation

--- client/src/Components/ClaimForm.jsx
+++ client/src/Components/ClaimForm.jsx
@@ -86,10 +86,7 @@ export default function ClaimForm({ onEmailSent }) {
     if (!formData.ein.trim()) {
       newErrors.ein = 'EIN is required';
       formIsValid = false;
-    } else if (formData.ein.trim().length !== 10) {
-      newErrors.ein = 'EIN must have exactly 9 digits';
-      formIsValid = false;
-    } else if (!/^\d{2}-\d{7}$|^\d{3}-\d{2}-\d{3}$/.test(formData.ein.trim())) {
+    } else if (!/^\d{2}-\d{6}$|^\d{3}-\d{2}-\d{4}$/.test(formData.ein.trim())) {
       newErrors.ein = 'EIN or SSN must be a valid number with format XX-XXXXXXX or XXX-XX-XXX';
       formIsValid = false;
     }
@@ -230,7 +227,7 @@ export default function ClaimForm({ onEmailSent }) {
     });
   };
 
-  
+
 
   // const [sign, setSign] = useState()
   const [url, setUrl] = useState()
