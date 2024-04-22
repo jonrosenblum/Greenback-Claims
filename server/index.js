7610afb6983e130e4ee24d57dde813d44977b4c4
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2024-04-22 15:14:52 -0400

    Fixing deploy

--- server/index.js
+++ server/index.js
@@ -58,5 +58,5 @@ app.use("/api/admin/", adminController);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on Port ${PORT}`);
 });
